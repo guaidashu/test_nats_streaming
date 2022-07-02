5496eaa71097e6807dc39d457de5244d98c75e1a
songjie <1023767856@qq.com>
2022-07-02 16:38:30 +0800

    添加异步推送的错误打印

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"strconv"
+	"sync"
 	"test_nats_streaming/app"
 	"test_nats_streaming/nats_streaming_libs"
+	"time"
 )
 
 func main() {
@@ -27,7 +29,20 @@ func main() {
 		// 进入选择
 		switch flagArr[0] {
 		case "pub":
-			app.Publisher(num)
+			// 启动推送
+			waitGroup := sync.WaitGroup{}
+			startTime := time.Now().UnixNano()
+			for i := 0; i < 100; i++ {
+				waitGroup.Add(1)
+				go func() {
+					app.Publisher(num)
+					waitGroup.Done()
+				}()
+			}
+			waitGroup.Wait()
+			endTime := time.Now().UnixNano()
+			result := float64(endTime-startTime) / 1000000000.0
+			fmt.Println(result)
 		case "pub_async":
 			app.PublishAsync(num)
 		case "sub":
